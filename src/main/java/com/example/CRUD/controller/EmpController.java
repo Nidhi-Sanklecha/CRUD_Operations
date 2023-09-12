e100ff56e52eaae6c25d9384366f5204f1542432
Nidhi-Sanklecha <nidhisanklecha8@gmail.com>
2023-09-12 10:37:51 +0530

    Added API documentation using Swagger

--- src/main/java/com/example/CRUD/controller/EmpController.java
+++ src/main/java/com/example/CRUD/controller/EmpController.java
@@ -2,11 +2,25 @@ package com.example.CRUD.controller;
 
 import com.example.CRUD.dao.EmpRepo;
 import com.example.CRUD.model.Employee;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 
 @RestController
+@OpenAPIDefinition(
+        info = @Info(
+                title="APIs",
+                version = "2.0.0",
+                description = "APIs for CRUD operation on employees",
+                contact = @Contact(
+                        name = "Nidhi",
+                        email = "int.Nidhi.Sanklecha@xpressbees.com"
+                )
+        )
+)
 public class EmpController {
     @Autowired
     private EmpRepo emp;
